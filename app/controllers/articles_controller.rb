72e27ce24c2bebcc8a73edd8312884a712285c15
Karina Miernik <karina.miernik@gmail.com>
2021-12-31 13:26:24 +0100

    added validation and flash

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,14 +2,22 @@ class ArticlesController < ApplicationController
   def index
     @articles = Article.all
   end
+  
   def show
     @article = Article.find(params[:id])
   end
+  
   def new
+    @article = Article.new
   end
+
   def create
     @article = Article.new(params.require(:article).permit(:title, :description))
-    @article.save
-    redirect_to @article
+    if @article.save
+      flash[:notice] = 'Article was created successfully'
+      redirect_to @article
+    else
+      render 'new'
+    end
   end
 end
