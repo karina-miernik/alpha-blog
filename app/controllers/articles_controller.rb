4c384a03138d070ba3380b91523ea7040004df30
Karina Miernik <karina.miernik@gmail.com>
2021-12-31 13:14:29 +0100

    added new/create method

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -5,5 +5,11 @@ class ArticlesController < ApplicationController
   def show
     @article = Article.find(params[:id])
   end
-
+  def new
+  end
+  def create
+    @article = Article.new(params.require(:article).permit(:title, :description))
+    @article.save
+    redirect_to @article
+  end
 end
