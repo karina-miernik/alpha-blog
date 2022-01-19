9996e388f8be84f46ef6e084b0bbedd725e9f5fa
Karina Miernik <karina.miernik@gmail.com>
2022-01-19 11:18:25 +0100

    signup form

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   root 'pages#home'
   get 'about', to: 'pages#about'
+  get 'signup', to: 'users#new'
+  post 'users', to: 'users#create'
   resources :articles
+  resources :users, except: [:new]
 end
